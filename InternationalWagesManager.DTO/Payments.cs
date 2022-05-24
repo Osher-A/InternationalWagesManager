dfddebf8a854b6270269153f0b29cbf679752425
Osher Moscovitch <oamoscovitch@gmail.com>
2022-05-25 00:31:10 +0100

    add DTO classes

---
+++ InternationalWagesManager.DTO/Payments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternationalWagesManager.DTO
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ InternationalWagesManager.DTO/SalaryComponents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternationalWagesManager.DTO
+{
+    public class SalaryComponents
+    {
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime Month { get; set; }
+        public float TotalHours { get; set; }
+        public float BonusHours { get; set; }
+        public float BonusWage { get; set; }
+        public float Expenses { get; set; }
+
+    }
+}
