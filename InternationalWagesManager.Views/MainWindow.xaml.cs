eb870eebaffb88ecfa95692465900038ff6978f1
Osher Moscovitch <oamoscovitch@gmail.com>
2022-06-08 00:26:11 +0100

    add initial pages

--- InternationalWagesManager.Views/MainWindow.xaml.cs
+++ InternationalWagesManager.Views/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using AutoMapper;
 using InternationalWagesManager.DAL;
 using InternationalWagesManager.Views.Pages;
+using MahApps.Metro.Controls;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InternationalWagesManager.Views
@@ -22,7 +23,7 @@ namespace InternationalWagesManager.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private IEmployeeRepository _employeeRepository;
         private IMapper _mapper;
@@ -35,7 +36,18 @@ namespace InternationalWagesManager.Views
 
         private void MainWindowFrame_Loaded(object sender , RoutedEventArgs e)
         {
-            MainWindowFrame.Content = new HomePage(_employeeRepository, _mapper);
+            // MainWindowFrame.Content = new HomePage(_employeeRepository, _mapper);
         }
+
+        private void TreeViewAddEmployee_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MainWindowFrame.Content = new EmployeeDetails();
+        }
+        private void TreeViewAddWorkConditions_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MainWindowFrame.Content = new WorkConditions();
+        }
+
+       
     }
 }
